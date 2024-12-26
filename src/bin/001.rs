35640bda6c7e9356dd47e8ee191d18ec40cde36d
Ozan Malci <ozan41094@gmail.com>
2024-12-26 17:13:05 +0300

    Add solution for Project Euler problem 1

---
+++ src/bin/001.rs
@@ -0,0 +1,16 @@
+use project_euler::project_euler_solution;
+
+project_euler_solution!(1);
+
+/// # Multiples of 3 and 5
+/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+/// The sum of these multiples is 23.
+/// 
+/// Find the sum of all the multiples of 3 or 5 below 1000.
+fn project_euler_1() -> i32
+{
+    // Example implementation
+    (1..1000)
+        .filter(|&num| num % 3 == 0 || num % 5 == 0)
+        .sum()
+}
