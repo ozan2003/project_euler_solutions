b198a5de977ddb5f940c5d57064b8247991bb00f
Ozan Malci <ozan41094@gmail.com>
2024-12-26 17:11:50 +0300

    Refactor output formatting in project_euler_solution macro

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -9,8 +9,8 @@ macro_rules! project_euler_solution {
                 let start = Instant::now();
                 let result = [<project_euler_ $number>]();
                 let duration = start.elapsed();
-                println!("Answer: {}", result);
-                println!("Time: {}ms", duration.as_nanos() as f64 / 1_000_000.0);
+                println!("answer: {}", result);
+                println!("took {}ms", duration.as_nanos() as f64 / 1_000_000.0);
             }
         }
     };
