95c71e8278e0a557ca371f54d9d20c5ec7065ec3
Ozan Malci <ozan41094@gmail.com>
2024-12-26 17:34:48 +0300

    Refactor timing output in project_euler_solution macro for improved readability.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,16 +2,14 @@
 macro_rules! project_euler_solution {
     ($number:expr) => {
         paste::paste! {
-            use std::time::Instant;
 
             fn main() -> Result<(), Box<dyn std::error::Error>>
             {
-                let start = Instant::now();
+                let start = std::time::Instant::now();
                 let result = [<project_euler_ $number>]();
-                let duration = start.elapsed();
+                let elapsed = start.elapsed();
                 println!("answer: {}", result);
-                println!("took {}ms", duration.as_nanos() as f64 / 1_000_000.0);
-
+                println!("took {:.2?}", elapsed);
                 Ok(())
             }
         }
