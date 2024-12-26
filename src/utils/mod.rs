52bb98db29afd61da90fc5a8e8841a730248f7e9
Ozan Malci <ozan41094@gmail.com>
2024-12-26 17:19:13 +0300

    Update main function to return Result type for possible error handling

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,13 +4,15 @@ macro_rules! project_euler_solution {
         paste::paste! {
             use std::time::Instant;
 
-            fn main()
+            fn main() -> Result<(), Box<dyn std::error::Error>>
             {
                 let start = Instant::now();
                 let result = [<project_euler_ $number>]();
                 let duration = start.elapsed();
                 println!("answer: {}", result);
                 println!("took {}ms", duration.as_nanos() as f64 / 1_000_000.0);
+
+                Ok(())
             }
         }
     };
