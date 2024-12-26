22af12f2bb03e0081a5c854d120d5e0fe2d0cbb0
Ozan Malci <ozan41094@gmail.com>
2024-12-26 17:39:22 +0300

    Add documentation for project_euler_solution macro to clarify functionality

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,17 @@
+/// A macro that generates a main function for Project Euler solutions.
+///
+/// This macro creates a standardized main function that:
+/// - Records the start time
+/// - Calls the solution function for a specific problem
+/// - Measures and prints the elapsed time
+/// - Prints the result
+///
+/// # Arguments
+///
+/// * `$number` - The problem number. Used to construct the function name
+///               in the format `project_euler_N` where N is the problem number.
+///
+/// Note: Requires the `paste` crate for identifier concatenation.
 #[macro_export]
 macro_rules! project_euler_solution {
     ($number:expr) => {
