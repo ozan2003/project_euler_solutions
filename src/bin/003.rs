b81be08e7a810489e84b17cda8e3c95301f8616f
Ozan Malci <ozan41094@gmail.com>
2024-12-27 12:38:20 +0300

    Add solution for Project Euler problem 003: Largest prime factor

---
+++ src/bin/003.rs
@@ -0,0 +1,17 @@
+use num_prime::nt_funcs::factorize;
+use project_euler::project_euler_solution;
+
+project_euler_solution!(003);
+
+/// # Largest prime factor
+/// The prime factors of 13195 are 5, 7, 13 and 29.
+///
+/// What is the largest prime factor of the number 600851475143?
+fn project_euler_003() -> u64
+{
+    const NUM: u64 = 600_851_475_143;
+
+    let factors = factorize(NUM);
+
+    *factors.last_key_value().unwrap().0
+}
