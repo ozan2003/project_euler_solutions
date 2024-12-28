27f91c993a11b2dbfd34835ceb731e88c123939d
Ozan Malci <ozan41094@gmail.com>
2024-12-28 14:37:16 +0300

    Rename solution file from 050.rs to 013.rs

--- src/bin/013.rs
+++ src/bin/013.rs
@@ -3,18 +3,18 @@ use project_euler::project_euler_solution;
 use std::fs::File;
 use std::io::{BufReader, prelude::*};
 
-project_euler_solution!(050);
+project_euler_solution!(013);
 
 /// # Large Sum
 /// Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 /// 
 /// / ... /
-fn project_euler_050() -> String
+fn project_euler_013() -> String
 {
     let proj_dir = std::env::current_dir().unwrap();
 
     let file =
-        File::open(format!("{}/data/050.txt", proj_dir.display())).expect("Couldn't find file.");
+        File::open(format!("{}/data/013.txt", proj_dir.display())).expect("Couldn't find file.");
     let buf = BufReader::new(file);
 
     let sum = buf.lines()
