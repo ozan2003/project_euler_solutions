e5a6a98179328884ef35a4b330fa874793f855fa
Ozan Malci <ozan41094@gmail.com>
2024-12-30 21:51:20 +0300

    add comments and add unit tests for next_permutation function

--- src/bin/024.rs
+++ src/bin/024.rs
@@ -2,17 +2,9 @@ use project_euler::project_euler_solution;
 
 project_euler_solution!(024);
 
-/// # Lexicographic Permutations
-///
-/// A permutation is an ordered arrangement of objects. For example, 3124 is one
-/// possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
-/// are listed numerically or alphabetically, we call it lexicographic order.
-/// The lexicographic permutations of 0, 1 and 2 are:
-///
-/// 012   021   102   120   201   210
-///
-/// What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4,
-/// 5, 6, 7, 8 and 9?
+// Repeatedly generates the next lexicographic permutation of the given slice.
+// Returns true if a new permutation was generated, and false if the slice is
+// already at the last permutation.
 fn next_permutation<T: Ord>(arr: &mut [T]) -> bool
 {
     let last = arr.len().saturating_sub(1);
@@ -46,6 +38,17 @@ fn next_permutation<T: Ord>(arr: &mut [T]) -> bool
     true
 }
 
+/// # Lexicographic Permutations
+///
+/// A permutation is an ordered arrangement of objects. For example, 3124 is one
+/// possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
+/// are listed numerically or alphabetically, we call it lexicographic order.
+/// The lexicographic permutations of 0, 1 and 2 are:
+///
+/// 012   021   102   120   201   210
+///
+/// What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4,
+/// 5, 6, 7, 8 and 9?
 fn project_euler_024() -> String
 {
     let mut digits = "0123456789".chars().collect::<Vec<_>>();
@@ -55,4 +58,39 @@ fn project_euler_024() -> String
     }
 
     digits.into_iter().collect()
+}
+
+#[test]
+fn test_next_permutation()
+{
+    let mut nums = vec![0, 1, 2];
+    assert!(next_permutation(&mut nums));
+    assert_eq!(nums, vec![0, 2, 1]);
+
+    assert!(next_permutation(&mut nums));
+    assert_eq!(nums, vec![1, 0, 2]);
+
+    assert!(next_permutation(&mut nums));
+    assert_eq!(nums, vec![1, 2, 0]);
+
+    assert!(next_permutation(&mut nums));
+    assert_eq!(nums, vec![2, 0, 1]);
+
+    assert!(next_permutation(&mut nums));
+    assert_eq!(nums, vec![2, 1, 0]);
+
+    assert!(!next_permutation(&mut nums)); // Last permutation
+    assert_eq!(nums, vec![2, 1, 0]);
+
+    let mut chars = vec!['a', 'b', 'c'];
+    assert!(next_permutation(&mut chars));
+    assert_eq!(chars, vec!['a', 'c', 'b']);
+
+    let mut single = vec![1];
+    assert!(!next_permutation(&mut single));
+    assert_eq!(single, vec![1]);
+
+    let mut empty: Vec<i32> = vec![];
+    assert!(!next_permutation(&mut empty));
+    assert_eq!(empty, vec![]);
 }
