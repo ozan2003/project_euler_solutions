7094cb3af647394e14ee9a3d40127922d30c5baf
Ozan Malci <ozan41094@gmail.com>
2025-01-11 15:01:42 +0300

    Refactor macro to streamline main function in Project Euler solutions

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -15,15 +15,12 @@
 #[macro_export]
 macro_rules! project_euler_solution {
     ($number:expr) => {
-        paste::paste! {
-            fn main()
-            {
-                let start = std::time::Instant::now();
-                let result = [<project_euler_ $number>]();
-                let elapsed = start.elapsed();
-                println!("answer: {}", result);
-                println!("took {:.2?}", elapsed);
-            }
+        fn main() {
+            let start = std::time::Instant::now();
+            let result = paste::paste!{ [<project_euler_ $number>]() };
+            let elapsed = start.elapsed();
+            println!("answer: {}", result);
+            println!("took {:.2?}", elapsed);
         }
     };
 }
