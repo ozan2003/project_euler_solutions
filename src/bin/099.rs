832273cca36c7f5672aa895300a11db6595336c4
Ozan Malci <ozan41094@gmail.com>
2025-01-11 14:54:43 +0300

    minor lint

--- src/bin/099.rs
+++ src/bin/099.rs
@@ -51,7 +51,7 @@ impl PartialOrd for Exponent
 /// However, confirming that 632382^518061 > 519432^525806 would be much more
 /// difficult, as both numbers contain over three million digits.
 ///
-/// Using base_exp.txt (right click and 'Save Link/Target As...'), a 22K text
+/// Using `base_exp.txt` (right click and 'Save Link/Target As...'), a 22K text
 /// file containing one thousand lines with a base/exponent pair on each line,
 /// determine which line number has the greatest numerical value.
 ///
