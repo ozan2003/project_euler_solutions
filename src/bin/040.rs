9fc2d8fd90ec35bff31f667e295e64f3b90582ca
Ozan Malci <ozan41094@gmail.com>
2025-01-25 15:09:10 +0300

    Improve documentation formatting

--- src/bin/040.rs
+++ src/bin/040.rs
@@ -3,16 +3,18 @@ use project_euler::project_euler_solution;
 project_euler_solution!(040);
 
 /// # Champernowne's Constant
-/// 
-/// An irrational decimal fraction is created by concatenating the positive integers:
-/// 
+///
+/// An irrational decimal fraction is created by concatenating the positive
+/// integers:
+///
 /// 0.12345678910`1`112131415161718192021...
-/// 
+///
 /// It can be seen that the 12th digit of the fractional part is 1.
-/// 
-/// If d_n represents the nth digit of the fractional part, find the value of the following expression:
-/// 
-/// d_1 × d_10 × d_100 × d_1000 × d_10000 × d_100000 × d_1000000
+///
+/// If `d_n` represents the nth digit of the fractional part, find the value of
+/// the following expression:
+///
+/// `d_1` * `d_10` * `d_100` * `d_1000` * `d_10000` * `d_100000` * `d_1000000`
 fn project_euler_040() -> u8
 {
     const UPPER_LIMIT: usize = 1_000_000;
