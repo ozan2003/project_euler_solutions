7f48123b3b1831c424dae20bda59d87ebc86848e
Ozan Malci <ozan41094@gmail.com>
2025-01-25 19:26:25 +0300

    Refactor calculation of square_of_sum

--- src/bin/006.rs
+++ src/bin/006.rs
@@ -6,7 +6,7 @@ fn project_euler_006() -> u32
 {
     const UPPER_LIMIT: i32 = 100;
 
-    let square_of_sum = i32::pow((1..=UPPER_LIMIT).sum(), 2);
+    let square_of_sum = (1..=UPPER_LIMIT).sum::<i32>().pow(2);
     let sum_of_square = (1..=UPPER_LIMIT).map(|num| num * num).sum();
 
     square_of_sum.abs_diff(sum_of_square)
