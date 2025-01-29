1e5aa25c173add0ae2aed621da5d69b4dd24110f
Ozan Malci <ozan41094@gmail.com>
2025-01-29 16:37:21 +0300

    Update documentation for circular primes to remove redundant comments

--- src/bin/035.rs
+++ src/bin/035.rs
@@ -41,14 +41,6 @@ fn rotations(mut num: u32) -> impl Iterator<Item = u32>
 /// 71, 73, 79, and 97.
 ///
 /// How many circular primes are there below one million?
-/// # Circular Primes
-/// The number, 197, is called a circular prime because all rotations of the
-/// digits: 197, 971, and 719, are themselves prime.
-///
-/// There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37,
-/// 71, 73, 79, and 97.
-///
-/// How many circular primes are there below one million?
 #[allow(clippy::explicit_iter_loop)]
 fn project_euler_035() -> usize
 {
