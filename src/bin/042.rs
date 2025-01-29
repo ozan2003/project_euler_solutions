a36d91b5fca8c3cd7aff7b1e47fdfd20ab7b4c9b
Ozan Malci <ozan41094@gmail.com>
2025-01-29 15:54:23 +0300

    lint

--- src/bin/042.rs
+++ src/bin/042.rs
@@ -29,14 +29,14 @@ fn word_value(word: &[u8]) -> i32
 
 /// # Coded Triangle Numbers
 ///
-/// The nth term of the sequence of triangle numbers is given by, t_n = 1/2 * n
+/// The nth term of the sequence of triangle numbers is given by, `t_n` = 1/2 * n
 /// * (n + 1); so the first ten triangle numbers are:
 ///
 /// 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
 ///
 /// By converting each letter in a word to a number corresponding to its
 /// alphabetical position and adding these values we form a word value. For
-/// example, the word value for SKY is 19 + 11 + 25 = 55 = t_10. If the word
+/// example, the word value for SKY is 19 + 11 + 25 = 55 = `t_10`. If the word
 /// value is a triangle number then we shall call the word a triangle word.
 ///
 /// Using data/042.txt a 16K text file containing nearly two-thousand
