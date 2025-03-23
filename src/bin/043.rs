69b27ed48dd0212faf7095c6a2d012da388cc251
Ozan Malci <ozan41094@gmail.com>
2025-03-23 19:07:33 +0300

    lint

--- src/bin/043.rs
+++ src/bin/043.rs
@@ -30,19 +30,19 @@ fn slice_divisibility(slice: &[u64]) -> bool
 /// Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we
 /// note the following:
 ///
-/// d_2d_3d_4=406 is divisible by 2
+/// `d_2d_3d_4=406` is divisible by 2
 ///
-/// d_3d_4d_5=063 is divisible by 3
+/// `d_3d_4d_5=063` is divisible by 3
 ///
-/// d_4d_5d_6=635 is divisible by 5
+/// `d_4d_5d_6=635` is divisible by 5
 ///
-/// d_5d_6d_7=357 is divisible by 7
+/// `d_5d_6d_7=357` is divisible by 7
 ///
-/// d_6d_7d_8=572 is divisible by 11
+/// `d_6d_7d_8=572` is divisible by 11
 ///
-/// d_7d_8d_9=728 is divisible by 13
+/// `d_7d_8d_9=728` is divisible by 13
 ///
-/// d_8d_9d_10=289 is divisible by 17
+/// `d_8d_9d_10=289` is divisible by 17
 ///
 ///
 /// Find the sum of all 0 to 9 pandigital numbers with this property.
