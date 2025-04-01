fd13b5eccd906d0f3a2ca7dec6299f4d295acce8
Ozan Malci <ozan41094@gmail.com>
2025-04-01 10:58:45 +0300

    add comments

--- src/bin/006.rs
+++ src/bin/006.rs
@@ -2,6 +2,21 @@ use project_euler::project_euler_solution;
 
 project_euler_solution!(006);
 
+/// Sum Square Difference
+///
+/// The sum of the squares of the first ten natural numbers is,
+///
+/// 1^2 + 2^2 + ... + 10^2 = 385
+///
+/// The square of the sum of the first ten natural numbers is,
+///
+/// (1 + 2 + ... + 10)^2 = 55^2 = 3025
+///
+/// Hence the difference between the sum of the squares of the first ten natural
+/// numbers and the square of the sum is 3025 - 385 = 2640.
+///
+/// Find the difference between the sum of the squares of the first one hundred
+/// natural numbers and the square of the sum.
 fn project_euler_006() -> u32
 {
     const UPPER_LIMIT: i32 = 100;
